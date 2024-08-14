88798350db26827815594e62fb54787e61c13032
Toby-Query <tobyquery1@gmail.com>
2024-08-14 22:15:21 +0200

    welcome screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:event_and_activities_app/screens/welcome.dart';
 import 'package:flutter/material.dart';
 
-
 void main() {
   runApp(const MyApp());
 }
@@ -13,11 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Events',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: const Welcome());
+      debugShowCheckedModeBanner: false,
+      title: 'Events',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: CodiaPage(),
+    );
   }
 }
